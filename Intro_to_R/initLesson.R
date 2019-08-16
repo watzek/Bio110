d8c6df08bbe5e57dff8105dd6c6b9443ec602899
Jeremy <jeremym@lclark.edu>
2019-08-16 14:17:06 -0700

    Update initLesson.R
    error in get_course_path, I think!

--- Intro_to_R/initLesson.R
+++ Intro_to_R/initLesson.R
@@ -3,4 +3,4 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
-data <- read.csv(file.path(.get_course_path(), "Bio110", "Bio110_lesson_two", "Handling_Visualizing_Data_data.csv"))
+data <- read.csv(file.path(get_course_path(), "Bio110", "Bio110_lesson_two", "Handling_Visualizing_Data_data.csv"))
