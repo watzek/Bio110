3b7ce08c85935be05eeba4d156efc56aebccbf3b
Jeremy <jeremym@lclark.edu>
2019-08-16 14:09:57 -0700

    updated content

---
+++ Intro_to_R/initLesson.R
@@ -0,0 +1,6 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+data <- read.csv(file.path(.get_course_path(), "Bio110", "Bio110_lesson_two", "Handling_Visualizing_Data_data.csv"))
