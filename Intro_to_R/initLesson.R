29da8f02db3a80b23da1159716a9188e83cce461
Jeremy <jeremym@lclark.edu>
2019-08-16 14:22:02 -0700

    defined .get_course_path in intro init file

--- Intro_to_R/initLesson.R
+++ Intro_to_R/initLesson.R
@@ -2,5 +2,9 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-
-data <- read.csv(file.path(get_course_path(), "Bio110", "Bio110_lesson_two", "Handling_Visualizing_Data_data.csv"))
+      .get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+data <- read.csv(file.path(.get_course_path(), "Bio110", "Bio110_lesson_two", "Handling_Visualizing_Data_data.csv"))
